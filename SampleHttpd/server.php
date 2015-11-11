15535e7bcd8abb7ad8fec108fa6e81714b6c0d7f
nemo <nemo@mulodo.com>
2015-11-11 13:32:59 +0900

    * modify    apply for loop      - re-lesten after finishing one session.

--- SampleHttpd/server.php
+++ SampleHttpd/server.php
@@ -3,28 +3,33 @@ namespace SampleHttpd;
 
 // CONFIGURATION
 $conf = array(
-    'listen_port' => 9999,
+    'LISTEN_PORT' => 9999,
+    'MAX_REQUEST' => 4096,
 );
 
 // CREATE A SOCKET
-$port = $conf['listen_port'];
+$port = $conf['LISTEN_PORT'];
 $sock = socket_create_listen($port);
 
-// ACCEPT (= Start Listen)
-$clientsock = socket_accept($sock);
 // LISTEN LOOP
-$data = null;
 while (true) {
-    $buf = socket_read($clientsock, 1024);
-    $data .= $buf;
-    if ($buf == "") {
-        break;
+
+    // ACCEPT (= Start Listen)
+    $clientsock = socket_accept($sock);
+    if ($clientsock === false) die("Fail to create socket\n");
+    // FOR ONE SESSION (other client will be waiting)
+    $data = null;
+    while (true && strlen($data) < $conf['MAX_REQUEST']) {
+        $buf = socket_read($clientsock, 1024);
+        $data .= $buf;
+        if ($buf == "") {
+            break;
+        }
     }
+    // CLOSE SOCKET
+    socket_close($clientsock);
+    // ECHO current session Data
+    echo $data;
 }
 
-// CLOSE SOCKET
-socket_close($clientsock);
-
-// ECHO Data
-echo $data;
 exit;
